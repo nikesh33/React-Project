1f325104c74f5e23f326e1bcbc6c96e99d00df0e
Nikesh Vishwakarma <nikeshvishwakarma33@gmail.com>
2024-06-29 16:19:58 +0530

    added components and routes with two ways

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from './App.jsx'
+import "./index.css";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+// // one way 
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       { path: "", element: <Home /> },
+//       { path: "about", element: <About /> },
+//       { path: "contact", element: <Contact /> },
+//     ],
+//   },
+// ]);
+
+// //two way
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <Route path="/" element={<Layout/>}>
+  <Route path="" element={<Home/>}/>
+  <Route path="about" element={<About/>}/>
+  <Route path="contact" element={<Contact/>}/>
+
+  </Route>
+  )
 )
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
